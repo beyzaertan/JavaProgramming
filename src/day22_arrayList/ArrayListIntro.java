0d8dbdbaa0d996530c5367fb7643d469915247cf
beyzaertan <104668542+beyzaertan@users.noreply.github.com>
2022-06-16 11:37:00 -0400

    Initial commit

---
+++ src/day22_arrayList/ArrayListIntro.java
@@ -0,0 +1,17 @@
+package day22_arrayList;
+
+import java.util.Arrays;
+
+public class ArrayListIntro {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        System.out.println(Arrays.toString(array));
+    }
+}

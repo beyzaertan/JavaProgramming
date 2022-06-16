62c365da3f297bb26a4e52fe504737a1c203a7cd
beyzaertan <104668542+beyzaertan@users.noreply.github.com>
2022-06-15 23:14:28 -0400

    Initial commit

---
+++ src/practice/hw1.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class hw1 {
+    public static void main(String[] args) {
+int i;
+int count = 0;
+       for(int j = 0; j < 5; j++) {
+           for ( i = 5; i > count; i--) {
+               System.out.print(i + " " );
+           }
+           count++;
+           System.out.println("\n");
+
+       }
+    }
+}

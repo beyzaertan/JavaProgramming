1c035df54c695a38629cd56391654a47b545e402
beyzaertan <104668542+beyzaertan@users.noreply.github.com>
2022-06-19 13:34:05 -0400

    Initial commit

---
+++ src/practice/hw2.java
@@ -0,0 +1,14 @@
+package practice;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class hw2 {
+    public static void main(String[] args) {
+        int[] arr = { 5, -2, 23, 7, 87, -42, 509 };
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(7);
+        numbers.add(3);
+        numbers.add(9);
+        numbers.add(-33);
+
+    }
+}

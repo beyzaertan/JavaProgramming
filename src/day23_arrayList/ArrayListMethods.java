134c7970ff951318e67bd460652dc202fdcaf703
beyzaertan <104668542+beyzaertan@users.noreply.github.com>
2022-06-19 09:30:07 -0400

    Initial commit

---
+++ src/day23_arrayList/ArrayListMethods.java
@@ -0,0 +1,14 @@
+package day23_arrayList;
+
+import java.util.Arrays;
+
+public class ArrayListMethods {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        array[0] = 100;
+
+        System.out.println(Arrays.toString(array));
+
+    }
+}

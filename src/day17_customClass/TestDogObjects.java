869e1aba090c74d35fa9ece61d6f81cd0fd3a9f4
beyzaertan <104668542+beyzaertan@users.noreply.github.com>
2022-06-21 18:46:38 -0400

    Initial commit

---
+++ src/day17_customClass/TestDogObjects.java
@@ -0,0 +1,12 @@
+package day17_customClass;
+
+public class TestDogObjects {
+    public static void main(String[] args) {
+
+        Dog dog3 = new Dog();
+        dog3.setInfo("Loki", "Chow chow", 'M', 3, "Medium","Yellow");
+
+        System.out.println(dog3);
+    }
+
+}
